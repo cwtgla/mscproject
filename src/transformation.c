cd6d31e140086d0cdf54aed9b3ade0da71ac511c
CWTGLA <2282467t@student.gla.ac.uk>
2018-08-08 16:02:55 +0100

    adding transformation implementation for cpu & opencl

--- src/transformation.c
+++ src/transformation.c
@@ -1,47 +1,140 @@
 #include <stdio.h>
+#include <dirent.h>
+#include <string.h>
 #include <stdlib.h>
-#include <time.h>
+//#include "zfp_example.h"
 
-//#define FILELOC "../data/simulation_data/one_half_sim.txt.clean"
-//#define FILELOC "../data/simulation_data/complete_sim.txt.clean"
-//#define FILELOC "../data/simulation_data/one_quarter_sim.txt.clean"
-//#define FILELOC "../data/simulation_data/one_third_sim.txt.clean"
-//#define FILELOC "../data/simulation_data/three_quarter_sim.txt.clean"
-#define FILELOC "../data/simulation_data/two_third_sim.txt.clean"
-#define LINECOUNT 2025000
+float globalMax = -10000.0f;
+float globalMin = 10000.0f;
 
-struct entry {
+struct rlEntry {
 	float value;
-	unsigned int count;
+	uint count;
 };
 
-/* Convert from (i,j,k) for a 3D array to flat array index
-rng - ranges i.e. (hb-lb)+1
-lb - lower bounds */
+/* convert from i,j,k notation for a 3D array to a flat index
+ * rng - ranges i.e. hl-lb+1
+ * lb - lower bounds
+ */
 inline unsigned int F3D2C(unsigned int i_rng, unsigned int j_rng, int i_lb, int j_lb, int k_lb, int ix, int jx, int kx) {
 	return (i_rng*j_rng*(kx-k_lb)+i_rng*(jx-j_lb)+ix+i_lb);
 }
 
-int main(void) {
-	//read floating points into array
-	FILE *contentFile;
-	float *content = calloc(LINECOUNT, sizeof(float));
-	contentFile = fopen(FILELOC, "r");
+//assumes 0'd out files array
+void getAbsFilenames(char *basedir, char *files[], char *extension) {
+	struct dirent *directoryEntry;
+	DIR *directory = opendir(basedir);
+	int pathLen = strlen(basedir)+1; //for null char
 
-	//while not EOF
-	int i = 0;
-	while (fscanf(contentFile, "%f", &content[i]) == 1) {
-		i++;
+	if(directory != NULL) {
+		int count = 0;
+		//whilst still iterating over an entry
+		while((directoryEntry = readdir(directory)) != NULL) {
+			//if the entry is of the desired extension
+			if(strstr(directoryEntry->d_name,extension) != NULL) {
+				files[count] = calloc((strlen(directoryEntry->d_name)+pathLen), sizeof(char));
+				strcat(files[count], basedir);
+				strcat(files[count], directoryEntry->d_name);
+				count++;
+			}
+		}
+	}
+}
+
+void runlengthStats(float *values, int count) {
+	struct rlEntry *entries = calloc(count, sizeof(struct rlEntry));
+	entries[0].count = 1;
+	entries[0].value = values[0];
+
+	int i;
+	int entryIndex = 0;
+	for(i = 1; i < count; i++) {
+		if(entries[entryIndex].value == values[i]) {
+			entries[entryIndex].count++;
+		} else {
+			entryIndex++;
+			entries[entryIndex].value = values[i];
+			entries[entryIndex].count = 1;
+		}
 	}
 
-	//for each index e.g. from index 0 to linecount-1	
-	printf("%d\n", F3D2C(150,150,0,0,0,150,0,0)); //start of all
-	printf("%d\n", F3D2C(150,150,0,0,0,1,0,0)); //last i ind
-	printf("%d\n", F3D2C(150,150,0,0,0,0,1,0));
-	printf("%d\n", F3D2C(150,150,0,0,0,0,0,1));
+	free(entries);
+	printf("\t\t*****RUNLENGTH STATS *****\n");
+	printf("\t\tNumber of indexes in runlength array %d, a net reduction in %d indexes\n\t\tSize in bytes of runlength array %lu bytes\n", entryIndex, count-entryIndex, sizeof(struct rlEntry)*entryIndex);
+}
 
-	//F3D2C iBounds, jBounds, iBase, jBase, kBase, iInd, jInd, kInd
-	free(content);	
+//Print stats from using zfp to compress values
+void zfpStats(float *values, int count) {
+	printf("\t\t\t*****ZFP STATS *****\n");
+	size_t compressedSize;
+//	compressedSize = compress(values, 150, 150, 90, 0.00, 0);
+	printf("\t\t\tSize after zfp compression %lu bytes\n", compressedSize);
+}
 
+//Get basic file stats (max,min,mean and number of values)
+void displayStats(char *filePath) {
+	FILE *contentFile;
+
+	contentFile = fopen(filePath, "r");
+	float *content = calloc(150*150*90, sizeof(float));
+	int i = 0;
+
+	while(fscanf(contentFile, "%f", &content[i]) == 1) {
+		i++;
+	}
+	fclose(contentFile);
+	
+	transform(content);
+	free(content);
+}
+
+void transform(float *content) {
+	
+	//for each index
+	//calc if on boundary
+	//update value
+	//
+}
+
+int main(int argc, char *argv[]) {
+	if(argc < 2) {
+		printf("Missing additional params, recieved: %d\n", argc);
+		return -1;
+	}
+	printf("Getting files in %s\n", argv[1]);
+	//Grab absolute path of all relevant files
+	char *files[10];
+	getAbsFilenames(argv[1], files, ".txt.clean");
+	
+	int i = 0;
+	printf("Found files..\n");
+	for(i = 0; i < 6; i++) {
+		printf("%d : %s\n", i+1, files[i]);
+	}
+	
+
+
+	//printf("Getting stats for files..\n");
+	//Get each files stats
+	//for(i = 0; i < 6; i++) {
+	displayStats(files[0]);
+	//}
+
+	//printf("Global stats..\n");
+	//printf("Largest value found across files: %f. Smallest value found: %f\n", globalMax, globalMin);
+
+	//struct dirent *directoryEntry;
+	//DIR *directory = opendir(argv[1]);
+
+	//if(directory == NULL) {
+//		printf("ERROR");
+//		return -1;
+//	}
+	
+	//read entries
+//	while((directoryEntry = readdir(directory)) != NULL) {
+//		printf("%s\n", directoryEntry->d_name);
+//	}
+//	closedir(directory);
 	return 0;
 }
